4a49863dd0cb019abeafeec4d189f0196a1142d1
Andre Igor <andre.igor@pd.griaule>
2022-11-16 13:42:40 -0300

    fixing fingernet mnt shape result

--- pytorch/src/FingernetDeploy.py
+++ pytorch/src/FingernetDeploy.py
@@ -21,6 +21,7 @@ from modelDeploy import MainNet
 import settings
 
 
+
 class DatasetImage(Dataset): # there are three mandatory functions: init, len, getitem
     def __init__(self, dataset, transform=None):
         # it gets the image true labels and set the preprocessing transformation
@@ -43,10 +44,15 @@ def GetBatches(dataset, batchsize, transform = None):
 
 def get_image_prep(image_shape):
     # it is important that the image dimensions are a multiple of 8
-    width, height = int(image_shape[0] / 8) * 8, int(image_shape[1] / 8) * 8
+    
+    # getting closest 8 multiple for x dimension
+    x_pad = 8 - image_shape[0] % 8
+    y_pad = 8 - image_shape[1] % 8
+    # width, height = int(image_shape[0] / 8) * 8, int(image_shape[1] / 8) * 8
     prep = transforms.Compose([
         transforms.ToPILImage(),
-        transforms.Resize((width,height)),
+        transforms.Pad((y_pad, x_pad, 0, 0), fill = 255),
+        # transforms.Resize((width,height)),
         transforms.ToTensor()
     ])
 
@@ -80,11 +86,7 @@ def main():
     batch_size = 1
     
 
-    dataset = DatasetImage(fileset, None)
-    image_shape = dataset[0].shape
-    prep = get_image_prep(image_shape)
-    dataloader = GetBatches(fileset, batch_size, prep)
-
+    ix = 0
 
     # applying model forward and saving to output folder
 
@@ -92,56 +94,63 @@ def main():
         os.mkdir(sys.argv[2])
     except FileExistsError:
         print(f'Warning: "{sys.argv[2]}" folder already exists. May overwrite files.\n')
+    
+    for ix,path in enumerate(fileset):
+        files = [path]
 
+        dataset = DatasetImage(files, None)
+        image_shape = dataset[0].shape
+        prep = get_image_prep(image_shape)
+        dataloader = GetBatches(files, batch_size, prep)
+        print(f'Applying model to image {ix}/{int(len(fileset)/batch_size)} ...')
+        with torch.no_grad():
+            for batch in dataloader:
+                            
+                tic = time.time()
+                
+                enh_img_real, ori_out_1, ori_out_2, seg_out, mnt_o_out, mnt_w_out, mnt_h_out, mnt_s_out = model(batch.to(device))
+                
+                
+                # ####################### POST PROCESSING ###########################
+                tic = time.time()
 
-    with torch.no_grad():
-        for ix, batch in enumerate(dataloader):
-            print(f'Applying model to image {ix + 1}/{int(len(fileset)/batch_size)} ...')
-                        
-            tic = time.time()
-            
-            enh_img_real, ori_out_1, ori_out_2, seg_out, mnt_o_out, mnt_w_out, mnt_h_out, mnt_s_out = model(batch.to(device))
-            
-            
-            # ####################### POST PROCESSING ###########################
-            tic = time.time()
+                # transforming torch tensors to numply to reuse fingernet original util functions
+                seg_out = seg_out.permute(0,2,3,1).detach().cpu().numpy()
+                mnt_o_out = mnt_o_out.permute(0,2,3,1).detach().cpu().numpy()
+                mnt_s_out = mnt_s_out.permute(0,2,3,1).detach().cpu().numpy()
+                mnt_w_out = mnt_w_out.permute(0,2,3,1).detach().cpu().numpy()
+                mnt_h_out = mnt_h_out.permute(0,2,3,1).detach().cpu().numpy()
+                enh_img_real = enh_img_real.permute(0,2,3,1).detach().cpu().numpy()
 
-            # transforming torch tensors to numply to reuse fingernet original util functions
-            seg_out = seg_out.permute(0,2,3,1).detach().cpu().numpy()
-            mnt_o_out = mnt_o_out.permute(0,2,3,1).detach().cpu().numpy()
-            mnt_s_out = mnt_s_out.permute(0,2,3,1).detach().cpu().numpy()
-            mnt_w_out = mnt_w_out.permute(0,2,3,1).detach().cpu().numpy()
-            mnt_h_out = mnt_h_out.permute(0,2,3,1).detach().cpu().numpy()
-            enh_img_real = enh_img_real.permute(0,2,3,1).detach().cpu().numpy()
+                # post processing segmentation mask
+                round_seg = np.round(np.squeeze(seg_out))
+                kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5, 5))
+                seg_out = cv2.morphologyEx(round_seg, cv2.MORPH_OPEN, kernel)
 
-            # post processing segmentation mask
-            round_seg = np.round(np.squeeze(seg_out))
-            kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5, 5))
-            seg_out = cv2.morphologyEx(round_seg, cv2.MORPH_OPEN, kernel)
+                # post processing minutia labels
+                mnt = label2mnt(np.squeeze(mnt_s_out)*np.round(np.squeeze(seg_out)), mnt_w_out, mnt_h_out, mnt_o_out, thresh=0.5)
+                mnt_nms = nms(mnt)
 
-            # post processing minutia labels
-            mnt = label2mnt(np.squeeze(mnt_s_out)*np.round(np.squeeze(seg_out)), mnt_w_out, mnt_h_out, mnt_o_out, thresh=0.5)
-            mnt_nms = nms(mnt)
+                # post processing orientation field
+                ori = torch_ori_highest_peak(ori_out_1)
+                ori = ((torch.argmax(ori, axis = 1)*2-90)/180.*np.pi).detach().cpu().numpy()
+                
+                # images saving parameters
+                img_name = str(fileset[ix]).split('/')[-1].split('.')[0]
+                set_name = '' # str(fileset[ix]).split('/')[2]
+                # img_size = np.array(batch.shape[2:], dtype=np.int32)/8*8 be careful here
+                img_size = image_shape
+                image = batch.permute(0,2,3,1).detach().cpu().numpy()
+                output_dir = sys.argv[2]
 
-            # post processing orientation field
-            ori = torch_ori_highest_peak(ori_out_1)
-            ori = ((torch.argmax(ori, axis = 1)*2-90)/180.*np.pi).detach().cpu().numpy()
-            
-            # images saving parameters
-            img_name = str(fileset[ix]).split('/')[-1].split('.')[0]
-            set_name = '' # str(fileset[ix]).split('/')[2]
-            img_size = np.array(batch.shape[2:], dtype=np.int32)/8*8
-            image = batch.permute(0,2,3,1).detach().cpu().numpy()
-            output_dir = sys.argv[2]
-
-            # Saving images
-            mnt_writer(mnt_nms, img_name, img_size, "%s/%s/%s.mnt"%(output_dir, set_name, img_name))
-            draw_ori_on_img(image, ori, seg_out, "%s/%s/%s_ori.png"%(output_dir, set_name, img_name))        
-            draw_minutiae(image, mnt_nms[:,:3], "%s/%s/%s_mnt.png"%(output_dir, set_name, img_name))
-            imageio.imsave("%s/%s/%s_enh.png"%(output_dir, set_name, img_name), (np.squeeze(enh_img_real)*ndimage.zoom(np.round(np.squeeze(seg_out)), [8,8], order=0)))
-            imageio.imsave("%s/%s/%s_seg.png"%(output_dir, set_name, img_name), (ndimage.zoom(np.round(np.squeeze(seg_out)), [8,8], order=0))) 
-            print(f'Post processing finised with sucess in {time.time() - tic} for image {ix + 1}/{int(len(fileset)/batch_size)}')
-            print('####################################')
+                # Saving images
+                mnt_writer(mnt_nms, img_name, img_size, "%s/%s/%s.mnt"%(output_dir, set_name, img_name))
+                draw_ori_on_img(image, ori, seg_out, "%s/%s/%s_ori.png"%(output_dir, set_name, img_name))        
+                draw_minutiae(image, mnt_nms[:,:3], "%s/%s/%s_mnt.png"%(output_dir, set_name, img_name))
+                imageio.imsave("%s/%s/%s_enh.png"%(output_dir, set_name, img_name), (np.squeeze(enh_img_real)*ndimage.zoom(np.round(np.squeeze(seg_out)), [8,8], order=0)))
+                imageio.imsave("%s/%s/%s_seg.png"%(output_dir, set_name, img_name), (ndimage.zoom(np.round(np.squeeze(seg_out)), [8,8], order=0))) 
+                print(f'Post processing finised with sucess in {time.time() - tic} for image {ix + 1}/{int(len(fileset)/batch_size)}')
+                print('####################################')
 
 
 
